b4dd22b8fbfbed302a3f9c2fadda6a25b98e65b5
sanjay <sshukla@nvidia.com>
2018-08-15 16:06:05 +0530

    Add code for Quadcopter take off task using DDPG

--- task.py
+++ task.py
@@ -24,11 +24,48 @@ class Task():
         self.action_size = 4
 
         # Goal
-        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 50.]) 
+        self.init_pose = np.array([0., 0., 10.,0,0,0])
 
     def get_reward(self):
         """Uses current pose of sim to return reward."""
-        reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+        #reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+        # initial reward - fraction of the z-velocity
+        reward = np.tanh(.7*self.sim.v[2])
+
+        
+        done = False
+        # reward if agent moving toward target and avoid overshooting target.
+
+        if ( self.sim.pose[2] < self.target_pos[2]):
+        	reward += 1.0
+        	done = True
+        else:
+        	reward -= 1.0
+        vz = self.sim.v[2]
+        vx = self.sim.v[0]
+        vy = self.sim.v[1]
+
+        #punish if xy velocities higher than z , th
+
+        if vx>vz or vy>vz:
+        	reward -=3.0
+
+        #to reward vertical velocity in takeoff task
+
+        if vz > 0:
+        	reward +=1.0
+        	done = True
+        else:
+        	reward -= 1
+        # penalize the downward movement relative to the starting position
+        if self.sim.pose[2] < self.init_pose[2]:
+        	reward -= 1
+        #this is to penalize crash
+        if done and self.sim.time < self.sim.runtime:
+        	reward = -1
+        #reward = max(1,min(-1, reward))
+
         return reward
 
     def step(self, rotor_speeds):
